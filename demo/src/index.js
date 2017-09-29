ac5859f332558cccb2ec9606db686a5e93a2267e
Justin Foley <justin@modweb.io>
2017-09-29 14:19:02 -0400

    basic bodymovin component

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 
 import Bodymovin from '../../src'
+import animationData from '../../src/checked_done_data';
 
+console.log(animationData);
 const Demo = () => (
   <div>
     <h1>react-bodymovin Demo</h1>
-    <Bodymovin/>
+    <Bodymovin animation={animationData} />
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import { lifecycle } from 'recompose';
+import bodymovin from 'bodymovin';
 
-const Bodymovin = () => (
-  <div>Bodymovin</div>
-)
-export default Bodymovin;
+let bodymovinRef = null;
+let animationData = null;
+
+const Bodymovin = ({animation}) => {
+  animationData = animation;
+  return (
+    <div id='bodymovin' ref={(element) => { bodymovinRef = element; }} style={{width: '100px'}}></div>
+  );
+}
+
+const lifecycleHooks = lifecycle({
+  componentDidMount: () => {
+    console.log(animationData);
+    bodymovin.loadAnimation({
+      container: bodymovinRef,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData,
+    })
+  }
+})
+export default lifecycleHooks(Bodymovin);
